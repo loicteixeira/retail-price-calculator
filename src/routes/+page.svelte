bd56e5e50da5398ecbd6a9c4fdb9b4f413ffccaa
Loïc Teixeira <hello@lta.me>
2025-01-10 18:00:58 +0100

    Styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -66,46 +66,58 @@
 	</form>
 
 	{#if results}
-		<table class="table-auto">
-			<thead class="uppercase text-gray-700">
-				<tr class="text-left align-text-top">
-					{#each results.columns as column}
-						<th
-							class="px-2 {column.children && 'text-center'}"
-							rowspan={column.children ? 1 : 2}
-							colspan={column.children ? column.children.length : 1}
-						>
-							{column.label}
-						</th>
-					{/each}
-				</tr>
-				<tr class="text-left align-text-top">
-					{#each results.columns as column}
-						{#if column.children}
-							{#each column.children as child}
-								<th class="px-2">{child}</th>
-							{/each}
-						{/if}
-					{/each}
-				</tr>
-			</thead>
-			<tbody>
-				{#each results.groups as group, index}
-					<tr class={index !== 0 ? 'border-t-2' : ''}>
-						<td class="px-2 py-1.5" rowspan={group.rows.length}>{group.label}</td>
-						{#each group.rows[0] as cell}
-							<td class="px-2 py-1.5">{cell}</td>
+		<div class="overflow-x-auto rounded border border-gray-300">
+			<table class="table-auto border-inherit">
+				<thead
+					class="text-md border-inherit bg-gray-100 text-center align-text-top uppercase tracking-wide text-gray-700"
+				>
+					<tr class="border-inherit">
+						{#each results.columns as column}
+							<th
+								class="border-inherit px-4 py-3 [&:not(:last-child)]:border-e"
+								colspan={column.children ? column.children.length : 1}
+							>
+								{column.label}
+							</th>
 						{/each}
 					</tr>
-					{#each group.rows.slice(1) as cells}
-						<tr>
-							{#each cells as cell}
-								<td class="px-2 py-1.5">{cell}</td>
+					<tr class="border-inherit">
+						{#each results.columns as column}
+							{#if column.children}
+								{#each column.children as child}
+									<th class="border border-inherit px-4 py-3">{child}</th>
+								{/each}
+							{:else}
+								<th class="border-b border-inherit px-4 py-3 [&:not(:last-child)]:border-e"
+									>&nbsp;</th
+								>
+							{/if}
+						{/each}
+					</tr>
+				</thead>
+				<tbody class="border-inherit">
+					{#each results.groups as group}
+						<tr class="border-b border-inherit">
+							<td
+								class="border-inherit px-4 py-3 [&:not(:last-child)]:border-e"
+								rowspan={group.rows.length}
+							>
+								{group.label}
+							</td>
+							{#each group.rows[0] as cell}
+								<td class="border-inherit px-4 py-3 [&:not(:last-child)]:border-e">{cell}</td>
 							{/each}
 						</tr>
+						{#each group.rows.slice(1) as cells}
+							<tr class="border-inherit [&:not(:last-child)]:border-b">
+								{#each cells as cell}
+									<td class="border-inherit px-4 py-3 [&:not(:last-child)]:border-e">{cell}</td>
+								{/each}
+							</tr>
+						{/each}
 					{/each}
-				{/each}
-			</tbody>
-		</table>
+				</tbody>
+			</table>
+		</div>
 	{/if}
 </div>
