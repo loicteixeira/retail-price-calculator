097fe1de0ea33b915898487f89707be4effbcced
Loïc Teixeira <hello@lta.me>
2025-01-12 14:43:01 +0100

    Wrap text in paragraphs

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,10 +9,12 @@
 <div class="prose mx-auto mb-8 px-4 sm:px-6 lg:px-8">
 	<h1>About</h1>
 
-	<span class="text-teal-700">Retail Price Calculator</span> is an
-	<a href="https://github.com/loicteixeira/retail-price-calculator">open-source tool</a> designed to
-	help you decide how to price your retail products, taking into account a base price, bundles
-	options and various fees.
+	<p>
+		<span class="text-teal-700">Retail Price Calculator</span> is an
+		<a href="https://github.com/loicteixeira/retail-price-calculator">open-source tool</a> designed to
+		help you decide how to price your retail products, taking into account a base price, bundles options
+		and various fees.
+	</p>
 
 	<p>
 		If you have any feedback (bug or feature request), please use this
@@ -23,7 +25,9 @@
 
 	<h3>Is my data safe?</h3>
 
-	The values you enter in the calculator are never uploaded to any server;<br />Your personal data
-	is never collected or used in any way while using this calculator.<br />You don’t need to create
-	an account or log in to use it.
+	<p>
+		The values you enter in the calculator are never uploaded to any server;<br />Your personal data
+		is never collected or used in any way while using this calculator.<br />You don’t need to create
+		an account or log in to use it.
+	</p>
 </div>
