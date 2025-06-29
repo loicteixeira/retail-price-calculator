7f5205721909759ed1c4c875ac9f1dcba9feef34
Loïc Teixeira <hello@lta.me>
2025-06-29 15:24:26 +0200

    Add more option to round to nearest

--- src/lib/components/Form/BundleOptionsFieldSet.svelte
+++ src/lib/components/Form/BundleOptionsFieldSet.svelte
@@ -86,6 +86,9 @@
 							<option value={2}>2</option>
 							<option value={5}>5</option>
 							<option value={10}>10</option>
+							<option value={15}>15</option>
+							<option value={20}>20</option>
+							<option value={25}>25</option>
 						</select></td
 					>
 					<td class="py-1.5">
